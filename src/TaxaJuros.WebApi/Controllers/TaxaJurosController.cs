c8cbeee63842cc0bf9fc1cc1bb5f45b9bdcd3a17
Rodrigo Prandi <rodrigoprandi@gmail.com>
2020-07-01 00:49:48 -0300

    Criando API fixa

---
+++ src/TaxaJuros.WebApi/Controllers/TaxaJurosController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TaxaJuros.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxaJurosController : ControllerBase
+    {
+        private readonly ILogger<TaxaJurosController> _logger;
+
+        public TaxaJurosController(ILogger<TaxaJurosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public decimal Get()
+        {
+            return 0.01M;
+        }
+    }
+}
